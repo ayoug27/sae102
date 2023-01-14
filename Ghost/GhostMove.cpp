7b5e462b287d37f92c09201556fffddc8af8b46d
Sandevistan <robin.lerouge@etu.univ-amu.fr>
2023-01-15 00:29:41 +0100

    changement direction famtome

--- Ghost/GhostMove.cpp
+++ Ghost/GhostMove.cpp
@@ -13,6 +13,9 @@
 #include "GhostH/GhostMove.h"
 #include "GhostH/iaghost.h"
 #include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -47,9 +50,20 @@ void RedGhostMove (Entity & RedGhost, Entity PacMan, CMat map){
         if(RedGhost.viewdirection == "Left"){intersection[1] = false;}
         MOVE(intersection,RedGhost.Pos.second,RedGhost.Pos.first,13,14,RedGhost);
     }
-
     if (RedGhost.state == "hide"){
-
+        vector<bool> intersection;
+        intersection = nbsortie(RedGhost.Pos.second,RedGhost.Pos.first,map);
+        if(RedGhost.viewdirection == "Top"){intersection[2] = false;}
+        if(RedGhost.viewdirection == "Right"){intersection[3] = false;}
+        if(RedGhost.viewdirection == "Bottom"){intersection[0] = false;}
+        if(RedGhost.viewdirection == "Left"){intersection[1] = false;}
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 6;}
+        if(PacMan.viewdirection == "Right"){xp -= 6;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 8;}
+        if(PacMan.viewdirection == "Left"){xp += 7;}
+        MOVE(intersection,RedGhost.Pos.second,RedGhost.Pos.first,xp,yp,RedGhost);
     }
 }
 
@@ -92,6 +106,19 @@ void PinkGhostMove (Entity & PinkGhost ,Entity PacMan, CMat map){
     }
 
     if (PinkGhost.state == "hide"){
+        vector<bool> intersection;
+        intersection = nbsortie(PinkGhost.Pos.second,PinkGhost.Pos.first,map);
+        if(PinkGhost.viewdirection == "Top"){intersection[2] = false;}
+        if(PinkGhost.viewdirection == "Right"){intersection[3] = false;}
+        if(PinkGhost.viewdirection == "Bottom"){intersection[0] = false;}
+        if(PinkGhost.viewdirection == "Left"){intersection[1] = false;}
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 9;}
+        if(PacMan.viewdirection == "Right"){xp -= 4;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 3;}
+        if(PacMan.viewdirection == "Left"){xp += 6;}
+        MOVE(intersection,PinkGhost.Pos.second,PinkGhost.Pos.first,xp,yp,PinkGhost);
 
     }
 }
@@ -107,6 +134,12 @@ void BlueGhostMove (Entity & BlueGhost,Entity PacMan, CMat map){
         if(BlueGhost.viewdirection == "Right"){intersection[3] = false;}
         if(BlueGhost.viewdirection == "Bottom"){intersection[0] = false;}
         if(BlueGhost.viewdirection == "Left"){intersection[1] = false;}
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 5;}
+        if(PacMan.viewdirection == "Right"){xp -= 3;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 1;}
+        if(PacMan.viewdirection == "Left"){xp += 4;}
         MOVE(intersection,BlueGhost.Pos.second,BlueGhost.Pos.first,PacMan.Pos.second,PacMan.Pos.first,BlueGhost);
     }
     if (BlueGhost.state == "flee"){
@@ -129,6 +162,19 @@ void BlueGhostMove (Entity & BlueGhost,Entity PacMan, CMat map){
     }
 
     if (BlueGhost.state == "hide"){
+        vector<bool> intersection;
+        intersection = nbsortie(BlueGhost.Pos.second,BlueGhost.Pos.first,map);
+        if(BlueGhost.viewdirection == "Top"){intersection[2] = false;}
+        if(BlueGhost.viewdirection == "Right"){intersection[3] = false;}
+        if(BlueGhost.viewdirection == "Bottom"){intersection[0] = false;}
+        if(BlueGhost.viewdirection == "Left"){intersection[1] = false;}
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 1;}
+        if(PacMan.viewdirection == "Right"){xp -= 4;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 6;}
+        if(PacMan.viewdirection == "Left"){xp += 3;}
+        MOVE(intersection,BlueGhost.Pos.second,BlueGhost.Pos.first,xp,yp,BlueGhost);
 
     }
 }
@@ -144,7 +190,13 @@ void OrangeGhostMove (Entity & OrangeGhost,Entity PacMan, CMat map){
         if(OrangeGhost.viewdirection == "Right"){intersection[3] = false;}
         if(OrangeGhost.viewdirection == "Bottom"){intersection[0] = false;}
         if(OrangeGhost.viewdirection == "Left"){intersection[1] = false;}
-        MOVE(intersection,OrangeGhost.Pos.second,OrangeGhost.Pos.first,PacMan.Pos.second,PacMan.Pos.first,OrangeGhost);
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 2;}
+        if(PacMan.viewdirection == "Right"){xp -= 0;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 3;}
+        if(PacMan.viewdirection == "Left"){xp += 1;}
+        MOVE(intersection,OrangeGhost.Pos.second,OrangeGhost.Pos.first,xp,yp,OrangeGhost);
 //                cout << OrangeGhost.viewdirection << endl;
     }
     if (OrangeGhost.state == "flee"){
@@ -167,6 +219,19 @@ void OrangeGhostMove (Entity & OrangeGhost,Entity PacMan, CMat map){
     }
 
     if (OrangeGhost.state == "hide"){
+        vector<bool> intersection;
+        intersection = nbsortie(OrangeGhost.Pos.second,OrangeGhost.Pos.first,map);
+        if(OrangeGhost.viewdirection == "Top"){intersection[2] = false;}
+        if(OrangeGhost.viewdirection == "Right"){intersection[3] = false;}
+        if(OrangeGhost.viewdirection == "Bottom"){intersection[0] = false;}
+        if(OrangeGhost.viewdirection == "Left"){intersection[1] = false;}
+        unsigned xp = PacMan.Pos.second;
+        unsigned yp = PacMan.Pos.first;
+        if(PacMan.viewdirection == "Top"){yp += 4;}
+        if(PacMan.viewdirection == "Right"){xp -= 7;}
+        if(PacMan.viewdirection == "Bottom"){yp -= 10;}
+        if(PacMan.viewdirection == "Left"){xp += 6;}
+        MOVE(intersection,OrangeGhost.Pos.second,OrangeGhost.Pos.first,xp,yp,OrangeGhost);
 
     }
 }
