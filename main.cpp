82a37c607d00f33dfed48aa50f8d5c8fcb7133eb
Sandevistan <robin.lerouge@etu.univ-amu.fr>
2023-01-14 20:59:12 +0100

    reduction main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-#include <iostream>
-#include <thread>
-#include <fstream>
-#include <map>
-#include "mingl/mingl.h"
-#include "mingl/gui/sprite.h"
-#include "mingl/shape/rectangle.h"
-#include "type.h"
 #include "game.h"
-
-using namespace std;
-
-
-int main()
-{
-    game();
-}
+int main(){game();}
 
